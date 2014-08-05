507b17a4f02bdbbe65afe187533030aef92f8484
Semyon Tikhonenko <yo@mail.com>
2014-08-05 19:31:50 +0300

    display songs list

--- src/com/example/FlyingDog/MyActivity.java
+++ src/com/example/FlyingDog/MyActivity.java
@@ -2,6 +2,9 @@ package com.example.FlyingDog;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import com.tiksem.media.local.AndroidAudioDataBase;
+import com.tiksem.media.local.LocalAudioDataBase;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +14,7 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        LocalAudioDataBase audioDataBase = new AndroidAudioDataBase(getContentResolver());
+        Log.i("MyActivity", audioDataBase.getSongs().toString());
     }
 }
