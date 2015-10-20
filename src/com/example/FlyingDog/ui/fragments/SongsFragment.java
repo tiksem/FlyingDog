6a2ac5225c944d37e5e1e2bcf3a3fc75b7eac2b2
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-10-20 21:43:38 +0300

    songs updating listview checked item position while next/prev

--- src/com/example/FlyingDog/ui/fragments/SongsFragment.java
+++ src/com/example/FlyingDog/ui/fragments/SongsFragment.java
@@ -1,11 +1,16 @@
 package com.example.FlyingDog.ui.fragments;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
 import com.example.FlyingDog.FlyingDog;
 import com.example.FlyingDog.ui.PlayListsActivity;
 import com.example.FlyingDog.ui.adapters.SongsAdapter;
 import com.tiksem.media.data.Audio;
 import com.tiksem.media.local.AudioDataBase;
+import com.tiksem.media.playback.AudioPlayerService;
+import com.tiksem.media.playback.PositionChangedListener;
 import com.utils.framework.CollectionUtils;
 import com.utilsframework.android.adapters.ViewArrayAdapter;
 import com.utilsframework.android.fragments.ListViewFragment;
@@ -20,13 +25,57 @@ public class SongsFragment extends AbstractPlayListFragment<Audio> {
     private List<String> urls;
 
     @Override
+    public void onStart() {
+        super.onStart();
+
+        final PlayListsActivity activity = getPlayListsActivity();
+        activity.executeWhenPlayBackServiceReady(new Runnable() {
+            @Override
+            public void run() {
+                updateListViewCheckedItemIfNeed(activity.getPlayBackService());
+            }
+        });
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final PlayListsActivity activity = getPlayListsActivity();
+        activity.executeWhenPlayBackServiceReady(new Runnable() {
+            @Override
+            public void run() {
+                onPlayBackServiceReady(activity.getPlayBackService());
+            }
+        });
+    }
+
+    private void onPlayBackServiceReady(final AudioPlayerService.Binder playBackService) {
+        playBackService.addPositionChangedListener(new PositionChangedListener() {
+            @Override
+            public void onPositionChanged() {
+                updateListViewCheckedItemIfNeed(playBackService);
+            }
+        });
+    }
+
+    private void updateListViewCheckedItemIfNeed(AudioPlayerService.Binder playBackService) {
+        if (playBackService.getPlayList() != urls) {
+            return;
+        }
+
+        int position = playBackService.getPosition();
+        getListView().setItemChecked(position, true);
+    }
+
+    @Override
     protected ViewArrayAdapter<Audio, ?> createAdapter() {
         return new SongsAdapter(getActivity());
     }
 
     @Override
-    protected List<Audio> createList() {
-        songs = audioDataBase.getSongs();
+    protected final List<Audio> createList() {
+        songs = getSongs();
         urls = CollectionUtils.transformNonCopy(songs, new CollectionUtils.Transformer<Audio, String>() {
             @Override
             public String get(Audio audio) {
@@ -34,6 +83,10 @@ public class SongsFragment extends AbstractPlayListFragment<Audio> {
             }
         });
         return songs;
+    }
+
+    protected List<Audio> getSongs() {
+        return audioDataBase.getSongs();
     }
 
     @Override
