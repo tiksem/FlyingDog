6002d251e79b262eecc9c98ddcc9aebc49745a14
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-10-21 15:34:52 +0300

    play/pause next prev controls integrated

--- src/com/example/FlyingDog/ui/fragments/AbstractPlayListFragment.java
+++ src/com/example/FlyingDog/ui/fragments/AbstractPlayListFragment.java
@@ -4,11 +4,15 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ToggleButton;
 import com.example.FlyingDog.FlyingDog;
 import com.example.FlyingDog.R;
 import com.example.FlyingDog.ui.PlayListsActivity;
 import com.tiksem.media.local.AudioDataBase;
 import com.tiksem.media.playback.AudioPlayerService;
+import com.tiksem.media.playback.StateChangedListener;
+import com.tiksem.media.playback.Status;
 import com.tiksem.media.ui.AudioPlaybackSeekBar;
 import com.utilsframework.android.fragments.Fragments;
 import com.utilsframework.android.fragments.ListViewFragment;
@@ -19,6 +23,8 @@ import com.utilsframework.android.view.GuiUtilities;
  */
 public abstract class AbstractPlayListFragment<T> extends ListViewFragment<T> {
     protected AudioDataBase audioDataBase;
+    private StateChangedListener stateChangedListener;
+    private AudioPlayerService.Binder playBackService;
 
     protected final PlayListsActivity getPlayListsActivity() {
         return (PlayListsActivity) getActivity();
@@ -43,9 +49,64 @@ public abstract class AbstractPlayListFragment<T> extends ListViewFragment<T> {
         activity.executeWhenPlayBackServiceReady(new Runnable() {
             @Override
             public void run() {
-                AudioPlaybackSeekBar seekBar = (AudioPlaybackSeekBar) view.findViewById(R.id.play_seek_bar);
-                seekBar.setPlayerBinder(activity.getPlayBackService());
+                onViewCratedAndPlayBackServiceReady(view, activity.getPlayBackService());
             }
         });
+    }
+
+    private void onViewCratedAndPlayBackServiceReady(View view, final AudioPlayerService.Binder playBackService) {
+        this.playBackService = playBackService;
+
+        AudioPlaybackSeekBar seekBar = (AudioPlaybackSeekBar) view.findViewById(R.id.play_seek_bar);
+        seekBar.setPlayerBinder(playBackService);
+
+        final ViewGroup playControls = (ViewGroup) view.findViewById(R.id.play_controls);
+        final ToggleButton playButton = (ToggleButton) view.findViewById(R.id.play);
+        View next = view.findViewById(R.id.next);
+        View prev = view.findViewById(R.id.prev);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBackService.togglePauseState();
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBackService.playNext();
+            }
+        });
+
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBackService.playPrev();
+            }
+        });
+
+        stateChangedListener = new StateChangedListener() {
+            @Override
+            public void onStateChanged(Status status) {
+                if (status == Status.PLAYING || status == Status.PAUSED) {
+                    playControls.setVisibility(View.VISIBLE);
+                    playButton.setChecked(status == Status.PLAYING);
+                    GuiUtilities.setEnabledForChildren(playControls, true);
+                } else {
+                    GuiUtilities.setEnabledForChildren(playControls, false);
+                }
+            }
+        };
+        playBackService.addStateChangedListener(stateChangedListener);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+
+        if (playBackService != null) {
+            playBackService.removeStateChangedListener(stateChangedListener);
+        }
     }
 }
