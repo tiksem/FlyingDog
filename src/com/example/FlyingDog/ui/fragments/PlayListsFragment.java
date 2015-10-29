6703e10106b67115e887e7830f1957f5fe1358f3
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-10-29 13:43:49 +0200

    add playlist logic implemented

--- src/com/example/FlyingDog/ui/fragments/PlayListsFragment.java
+++ src/com/example/FlyingDog/ui/fragments/PlayListsFragment.java
@@ -1,5 +1,9 @@
 package com.example.FlyingDog.ui.fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import com.example.FlyingDog.R;
 import com.example.FlyingDog.network.RequestManager;
 import com.example.FlyingDog.ui.Level;
 import com.example.FlyingDog.ui.PlayListsActivity;
@@ -8,7 +12,11 @@ import com.tiksem.media.data.Audio;
 import com.tiksem.media.data.PlayList;
 import com.tiksem.media.data.PlayLists;
 import com.utilsframework.android.adapters.ViewArrayAdapter;
+import com.utilsframework.android.threading.OnFinish;
+import com.utilsframework.android.threading.ThrowingRunnable;
+import com.utilsframework.android.view.Alerts;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,6 +24,40 @@ import java.util.List;
  * Created by stykhonenko on 27.10.15.
  */
 public class PlayListsFragment extends AbstractAudioDataFragment<PlayList> {
+    private void createPlayList(final String playListName) {
+        getRequestManager().execute(new ThrowingRunnable<IOException>() {
+            @Override
+            public void run() throws IOException {
+                audioDataBase.addPlayList(playListName);
+            }
+        }, new OnFinish<IOException>() {
+            @Override
+            public void onFinish(IOException e) {
+                updateNavigationListWithLastFilter();
+            }
+        });
+    }
+
+    private void showAddPlayListDialog() {
+        Alerts.showAlertWithInput(getActivity(), R.string.add_playlist_message, new Alerts.OnInputOk() {
+            @Override
+            public void onOk(String playListName) {
+                createPlayList(playListName);
+            }
+        });
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showAddPlayListDialog();
+            }
+        });
+    }
+
     @Override
     protected List<PlayList> createList() {
         List<PlayList> localPlayLists = audioDataBase.getPlayLists();
@@ -46,5 +88,10 @@ public class PlayListsFragment extends AbstractAudioDataFragment<PlayList> {
                 throw new UnsupportedOperationException("Unsupported internet playlist");
             }
         }
+    }
+
+    @Override
+    protected int getRootLayout() {
+        return R.layout.playlists_fragment;
     }
 }
