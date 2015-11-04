4e4a8abd919bb2182485206e56bd7fed8d5f7f93
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-11-04 22:38:58 +0200

    MediaPlayer IllegalStateException fixed

--- src/com/example/FlyingDog/network/FlyingDogRequestExecutor.java
+++ src/com/example/FlyingDog/network/FlyingDogRequestExecutor.java
@@ -7,17 +7,26 @@ import com.utilsframework.android.ExecuteTimeLogger;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by stykhonenko on 28.10.15.
  */
 public class FlyingDogRequestExecutor extends GetRequestExecutor {
+    private static AtomicInteger requestsCount = new AtomicInteger(0);
+
     @Override
     public String executeRequest(String url, Map<String, Object> args) throws IOException {
+        Log.i(RequestManager.TAG, "requests count = " + requestsCount.incrementAndGet());
         Log.i(RequestManager.TAG, "url = " + Network.getUrl(url, args));
         ExecuteTimeLogger.timeStart("request");
-        String result = super.executeRequest(url, args);
-        ExecuteTimeLogger.timeEnd("request", RequestManager.TAG);
+        String result;
+        try {
+            result = super.executeRequest(url, args);
+        } finally {
+            ExecuteTimeLogger.timeEnd("request", RequestManager.TAG);
+            Log.i(RequestManager.TAG, "requests count = " + requestsCount.decrementAndGet());
+        }
         return result;
     }
 }
