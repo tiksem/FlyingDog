d49ee91b0bb6217dc2539486a3cf4b69dd1d90df
Semen Tykhonenko <stykhonenko@cogniance.com>
2015-12-01 19:40:06 +0200

    reload functionality introduced

--- src/com/tiksem/FlyingDog/ui/fragments/SongsYouMayLikeFragment.java
+++ src/com/tiksem/FlyingDog/ui/fragments/SongsYouMayLikeFragment.java
@@ -1,8 +1,16 @@
 package com.tiksem.FlyingDog.ui.fragments;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AbsListView;
 import com.tiksem.FlyingDog.network.RequestManager;
 import com.tiksem.FlyingDog.services.FlyingDogSongsYouMayLikeService;
+import com.tiksem.SongsYouMayLike.R;
 import com.tiksem.media.data.Audio;
 import com.utils.framework.collections.InfiniteLoadingNavigationList;
 import com.utils.framework.collections.NavigationList;
@@ -17,6 +25,7 @@ public class SongsYouMayLikeFragment extends SongsOfFragment {
     private static final int DISTANCE_TO_LOAD_NEXT_PAGE = 50;
     private FlyingDogSongsYouMayLikeService.Binder binder;
     private Services.UnBinder unBinder;
+    private MenuItem updateItem;
 
     @Override
     public void onAttach(Activity activity) {
@@ -60,5 +69,60 @@ public class SongsYouMayLikeFragment extends SongsOfFragment {
     @Override
     protected boolean alwaysGetListFromInternet() {
         return true;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.update, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+
+        updateItem = menu.findItem(R.id.action_update);
+        updateItem.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                binder.reload();
+                updateNavigationListWithLastFilter();
+                return true;
+            }
+        });
+
+        refreshUpdateItemVisibility();
+    }
+
+    private void refreshUpdateItemVisibility() {
+        if (updateItem == null) {
+            return;
+        }
+
+        AbsListView listView = getListView();
+        if (listView != null) {
+            updateItem.setVisible(listView.getVisibility() == View.VISIBLE);
+        } else {
+            updateItem.setVisible(false);
+        }
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        refreshUpdateItemVisibility();
+    }
+
+    @Override
+    protected void onNavigationListChanged(NavigationList<Audio> navigationList) {
+        super.onNavigationListChanged(navigationList);
+        refreshUpdateItemVisibility();
+    }
+
+    @Override
+    protected void onListViewIsShown() {
+        super.onListViewIsShown();
+        refreshUpdateItemVisibility();
+    }
+
+    @Override
+    protected void onEmptyViewIsShown() {
+        super.onEmptyViewIsShown();
+        refreshUpdateItemVisibility();
     }
 }
